0a2f5b59cde68ee8c0ececddd18ca76a7348a6a2
caiorodrigues <caiorodrigues@hotmail.com>
2015-08-27 14:09:01 -0300

    q

---
+++ TestePrimefaces/src/main/java/model/UsuarioModel.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.io.Serializable;
+
+public class UsuarioModel implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long codigo;
+	private String nome;
+	private String senha;
+	private String email;
+	private String status;	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioModel other = (UsuarioModel) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+}
