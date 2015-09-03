5533e7a60fa0eecf5e59afbdae43194237ba893a
caiorodrigues <caiorodrigues@hotmail.com>
2015-09-03 14:56:47 -0300

    Primeiro commit do projeto de controle de ocorrencias

---
+++ ControleOcorrencias/src/main/java/br/com/caiorodrigues/controleocorrencias/util/AppContextListener.java
@@ -0,0 +1,24 @@
+package br.com.caiorodrigues.controleocorrencias.util;
+/*package com.algaworks.pedidovenda.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+*//**
+ * @author TI-Caio
+ *
+ *//*
+@WebListener
+public class AppContextListener implements ServletListenertenerContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		System.setProperty("org.apache.el.parser.COERCE_TO_ZERO", "false");
+	}
+
+}*/
